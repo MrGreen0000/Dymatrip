d650f6b43c91fba31516c3f1183c87971f7e6847
Naji Mouflih <91195806+MrGreen0000@users.noreply.github.com>
2024-04-12 14:37:19 +0200

    update

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,21 @@ const app = express();
 const path = require("path");
 const City = require("./models/city.model");
 const Trip = require("./models/trip.model");
+const e = require("express");
+const multer = require("multer");
+const subpath = "/public/assets/images/activities";
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, subpath));
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({
+  storage,
+});
 
 app.use(cors());
 mongoose.set("debug", true);
@@ -21,6 +36,7 @@ mongoose
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 
+// get cities
 app.get("/api/cities", async (req, res) => {
   try {
     const cities = await City.find({}).exec();
@@ -30,6 +46,7 @@ app.get("/api/cities", async (req, res) => {
   }
 });
 
+// get trips
 app.get("/api/trips", async (req, res) => {
   try {
     const trips = await Trip.find({}).exec();
@@ -39,6 +56,7 @@ app.get("/api/trips", async (req, res) => {
   }
 });
 
+// create trip
 app.post("/api/trip", async (req, res) => {
   try {
     const body = req.body;
@@ -49,6 +67,7 @@ app.post("/api/trip", async (req, res) => {
   }
 });
 
+// update trip
 app.put("/api/trip", async (req, res) => {
   try {
     const body = req.body;
@@ -58,6 +77,51 @@ app.put("/api/trip", async (req, res) => {
     res.json(trip);
   } catch (e) {
     res.status(500).json(e);
+  }
+});
+
+// add activity to a city
+app.post("/api/city/:cityId/activity", async (req, res) => {
+  try {
+    const cityId = req.params.cityId;
+    const activity = req.body;
+    const city = await City.findOneAndUpdate(
+      { _id: cityId },
+      { $push: { activities: activity } },
+      {
+        new: true,
+      }
+    ).exec();
+    setTimeout(() => {
+      res.json(city);
+    }, 3000);
+  } catch (e) {
+    res.status(500).json(e);
+  }
+});
+
+// verify uniqueness of a trip
+app.get(
+  "/api/city/:cityId/activities/verify/:activityName",
+  async (req, res) => {
+    const { cityId, activityName } = req.params;
+    const city = await City.findById(cityId).exec();
+    const index = city.activities.findIndex(
+      (activity) => activity.name === activityName
+    );
+    index === -1
+      ? res.json("Ok")
+      : res.status(400).json("L’activité existe déjà");
+  }
+);
+
+// upload activity image
+app.post("/api/activity/image", upload.single("activity"), (req, res, next) => {
+  try {
+    const publicPath = `http://localhost/public/assets/images/activities/${req.file.originalname}`;
+    res.json(publicPath || "error");
+  } catch (e) {
+    next(e);
   }
 });
 
